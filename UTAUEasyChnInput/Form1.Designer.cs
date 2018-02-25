02c6e50fe69b9997854ae7f37c66b022ea9674b8
Milkey Tan <819774989@qq.com>
2018-02-25 16:09:54 +0800

    添加项目文件。

---
+++ UTAUEasyChnInput/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace UTAUEasyChnInput
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.textBoxLyrics = new System.Windows.Forms.TextBox();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.ItemHeight = 15;
+            this.listBox.Location = new System.Drawing.Point(13, 13);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(122, 139);
+            this.listBox.TabIndex = 0;
+            // 
+            // textBoxLyrics
+            // 
+            this.textBoxLyrics.Location = new System.Drawing.Point(142, 13);
+            this.textBoxLyrics.Multiline = true;
+            this.textBoxLyrics.Name = "textBoxLyrics";
+            this.textBoxLyrics.Size = new System.Drawing.Size(379, 139);
+            this.textBoxLyrics.TabIndex = 1;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(13, 159);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(122, 26);
+            this.buttonSave.TabIndex = 2;
+            this.buttonSave.Text = "保存";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.Location = new System.Drawing.Point(399, 158);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(122, 26);
+            this.buttonOK.TabIndex = 3;
+            this.buttonOK.Text = "确定";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            this.buttonOK.Click += new System.EventHandler(this.ButtonOK_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(533, 193);
+            this.Controls.Add(this.buttonOK);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.textBoxLyrics);
+            this.Controls.Add(this.listBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.TextBox textBoxLyrics;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonOK;
+    }
+}
+
---
+++ UTAUEasyChnInput/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.International.Converters.PinYinConverter;
+
+namespace UTAUEasyChnInput
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonOK_Click(object sender, EventArgs e)
+        {
+            listBox.Items.Clear();
+            textBoxLyrics.Text.Replace(" ","");
+            foreach (char itemWord in textBoxLyrics.Text)
+            {
+                string pinyinStr = new ChineseChar(itemWord).Pinyins[0].ToString();
+                listBox.Items.Add(Regex.Replace(pinyinStr, @"\d", "").ToLower());
+            }
+        }
+    }
+}
